7ddc65a35536f4e1369a05c1764b926b04e5de8e
Denilson de León <denii.grifon@gmail.com>
2019-05-13 13:15:42 -0600

    Versión 7.1
    - Agregado COPYRIGHT
    - Se agrega el diagrama UML
    - Se agrega el manual de usuario
    - Se agrega el manual técnico

--- src/machineWar/Principal.java
+++ src/machineWar/Principal.java
@@ -1,5 +1,9 @@
 package machineWar;
-
+/**
+ * HECHO POR DENILSON DE LEON, 201830313
+ * PROTEGIDO CON COPYRIGHT
+ * @author guatemla
+ */
 public class Principal {
     public static void main(String[] args) {
         Correr correr = new Correr();
